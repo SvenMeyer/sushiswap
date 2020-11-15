2b39341c57c28b4c4899bbc1e7424e77242a06b5
Sven Meyer <git_dev@svenmeyer.com>
2020-11-16 00:00:46 +1100

    add WETH contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,29 +8,44 @@ const SushiBar = artifacts.require('SushiBar.sol');
 const SushiMaker = artifacts.require('SushiMaker.sol');
 const Migrator = artifacts.require('Migrator.sol');
 
-module.exports = async function(deployer, _network, addresses) {
+module.exports = async function(deployer, network, addresses) {
   const [admin, _] = addresses;
 
-  await deployer.deploy(WETH);
-  const weth = await WETH.deployed();
-  const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
-  const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
+  const WETH_address = {
+    main    : '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    ropsten : '0xc778417e063141139fce010982780140aa0cd5ab',
+    rinkeby : '0xc778417e063141139fce010982780140aa0cd5ab',
+    kovan   : '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+    private : ''
+  }
 
-  await deployer.deploy(Factory, admin);
-  const factory = await Factory.deployed();
-  await factory.createPair(weth.address, tokenA.address);
-  await factory.createPair(weth.address, tokenB.address);
-  await deployer.deploy(Router, factory.address, weth.address);
-  const router = await Router.deployed();
+  if (WETH_address[network] === '') {
+    await deployer.deploy(WETH);
+    const weth = await WETH.deployed();
+  } else {
+    const weth = WETH.at(WETH_address[network]);
+  }
+
+  console.log("using WETH deployed at ", network , ' - ' , weth.address); // #INFO
+
+  // const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
+  // const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
 
   await deployer.deploy(LadzToken);
   const ladzToken = await LadzToken.deployed();
+
+  await deployer.deploy(Factory, admin);
+  const factory = await Factory.deployed();
+  await factory.createPair(weth.address, ladzToken.address);
+  // await factory.createPair(weth.address, tokenB.address);
+  await deployer.deploy(Router, factory.address, weth.address);
+  const router = await Router.deployed();
 
   await deployer.deploy(
     MasterChef,
     ladzToken.address,
     admin,
-    web3.utils.toWei('100'),
+    web3.utils.toWei('2'),
     1,
     1
   );
